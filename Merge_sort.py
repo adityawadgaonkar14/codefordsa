1a1c7aed9b22b1a29b53a7d04d2f46765ec697b7
adityawadgaonkar14 <adityawadgaonkar14@gmail.com>
2025-11-08 01:34:58 +0530

    Update Merge_sort.py

--- Merge_sort.py
+++ Merge_sort.py
@@ -1,28 +1,48 @@
 def merge_sort(arr):
     if len(arr) > 1:
-        mid = len(arr)//2
-        left = arr[:mid]
-        right = arr[mid:]
+        mid = len(arr) // 2  # Find the middle index
+        left_half = arr[:mid]  # Divide into left half
+        right_half = arr[mid:]  # Divide into right half
 
-        merge_sort(left)
-        merge_sort(right)
+        # Recursive call to sort both halves
+        merge_sort(left_half)
+        merge_sort(right_half)
 
-        i = j = k = 0
-        while i < len(left) and j < len(right):
-            if left[i] < right[j]:
-                arr[k] = left[i]
+        i = j = k = 0  # i for left_half, j for right_half, k for main array
+
+        # Merge the sorted halves
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
                 i += 1
             else:
-                arr[k] = right[j]
+                arr[k] = right_half[j]
                 j += 1
             k += 1
 
-        while i < len(left):
-            arr[k] = left[i]
+        # Copy remaining elements of left_half, if any
+        while i < len(left_half):
+            arr[k] = left_half[i]
             i += 1
             k += 1
 
-        while j < len(right):
-            arr[k] = right[j]
+        # Copy remaining elements of right_half, if any
+        while j < len(right_half):
+            arr[k] = right_half[j]
             j += 1
             k += 1
+
+
+# -------- MAIN PROGRAM --------
+n = int(input("Enter number of online orders: "))
+orders = []
+
+for i in range(n):
+    time = int(input(f"Enter estimated delivery time for order {i+1} (in minutes): "))
+    orders.append(time)
+
+print("\nOriginal delivery times:", orders)
+
+merge_sort(orders)
+
+print("Sorted delivery times (Quickest first):", orders)
