90fcdd686e2b31eecbbd49c42da79480cefd8625
adityawadgaonkar14 <adityawadgaonkar14@gmail.com>
2025-11-08 01:23:02 +0530

    Update heapSorting.py

--- heapSorting.py
+++ heapSorting.py
@@ -1,45 +1,38 @@
 def heapify(arr, n, i):
     largest = i        # Initialize largest as root
-    left = 2 * i + 1   # Left child index
-    right = 2 * i + 2  # Right child index
+    left = 2 * i + 1   # left child
+    right = 2 * i + 2  # right child
 
-    # See if left child exists and is greater than root
+    # If left child exists and is greater than root
     if left < n and arr[left] > arr[largest]:
         largest = left
 
-    # See if right child exists and is greater than current largest
+    # If right child exists and is greater than largest so far
     if right < n and arr[right] > arr[largest]:
         largest = right
 
-    # Change root if needed
+    # If largest is not root, swap and continue heapifying
     if largest != i:
         arr[i], arr[largest] = arr[largest], arr[i]
-        # Heapify the root again
         heapify(arr, n, largest)
 
 
 def heap_sort(arr):
     n = len(arr)
 
-    # Build a maxheap
+    # Build a max heap (rearrange array)
     for i in range(n // 2 - 1, -1, -1):
         heapify(arr, n, i)
 
-    # Extract elements one by one
+    # One by one extract elements
     for i in range(n - 1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i]  # Swap
+        arr[i], arr[0] = arr[0], arr[i]   # swap
         heapify(arr, i, 0)
 
 
-# Main program
-arr = []
-n = int(input("Enter the number of elements: "))
-print("Enter elements:")
+# ---- Main Program ----
+arr = list(map(int, input("Enter numbers separated by space: ").split()))
 
-for i in range(n):
-    num = int(input())
-    arr.append(num)
-
-print("\nOriginal Array:", arr)
+print("Original array:", arr)
 heap_sort(arr)
-print("Sorted Array (Ascending order):", arr)
+print("Sorted array in ascending order:", arr)
